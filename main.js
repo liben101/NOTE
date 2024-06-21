e9cce902bf3649263d5d58c38a49d7fe98bd0ec9
ben.li <53158397+liben101@users.noreply.github.com>
2024-06-21 20:14:50 +0800

    1

---
+++ main.js
@@ -0,0 +1,28 @@
+const MENUS_1 = [
+    {
+        icon: ``,
+        title: '和利时DCS',
+        children: [
+            {
+                icon: ``,
+                title: '主控制器',
+                src1: './和利时_主控制器单元1.drawio.html'
+            }
+        ]
+    }, {
+        icon: ``,
+        title: 'MES相关',
+        children: [
+            {
+                icon: ``,
+                title: 'MES上下游',
+                src1: './MES上下游1.drawio.html'
+            },
+            {
+                icon: ``,
+                title: 'SQL相关',
+                src1: './SQL相关.drawio.html'
+            }
+        ]
+    },
+]
