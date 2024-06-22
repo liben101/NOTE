4a0191e256505c4fe4f37e07546d2fd5bb7d5d7d
lb <714376485@qq.com>
2024-06-22 11:42:03 +0800

    1

--- main.js
+++ main.js
@@ -16,12 +16,12 @@ const MENUS_1 = [
             {
                 icon: ``,
                 title: 'MES上下游',
-                src1: '/drawio/MES上下游1.drawio.html'
+                src1: './drawio/MES上下游1.drawio.html'
             },
             {
                 icon: ``,
                 title: 'SQL相关',
-                src1: '/drawio/SQL相关.drawio.html'
+                src1: './drawio/SQL相关.drawio.html'
             }
         ]
     },{
@@ -31,11 +31,11 @@ const MENUS_1 = [
             {
                 icon: ``,
                 title: '基本',
-                src1: '/drawio/中控/组态软件基本.drawio.html'
+                src1: './drawio/中控/组态软件基本.drawio.html'
             },{
                 icon: ``,
                 title: 'PID功能块的对比',
-                src1: '/drawio/中控/PID功能块的对比.drawio.html'
+                src1: './drawio/中控/PID功能块的对比.drawio.html'
             },
            
         ]
