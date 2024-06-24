618375729c789f93ab78f7102ee90f83ee7f336e
lb <714376485@qq.com>
2024-06-24 15:33:11 +0800

    1

--- index.mjs
+++ index.mjs
@@ -23,11 +23,11 @@ $(document).ready(async function() {
         // 遍历主菜单
         MENUS_1.forEach(item => {
     
-            let menu_1_item = document.createElement('div')
-            menu_1_item.className = 'top-header-item'
-            menu_1_item.innerHTML = `${item.title} 🔽`
-    
-    
+            var menu_1_item = $('<div>', {
+                class: 'top-header-item',
+                text: item.title
+            });
+
             // ------------------------------------------------
     
             let menu_1 = $('#children-header-1')
@@ -35,14 +35,14 @@ $(document).ready(async function() {
             // _chil是顶部子菜单列表
             let _chil = item.children
     
-            menu_1_item.addEventListener('click', (_e) => {
+            menu_1_item.click((_e) => {
     
                 if (ACTIVE_TOP_HEADER) {
-                    ACTIVE_TOP_HEADER.classList.remove('active')
+                    ACTIVE_TOP_HEADER.removeClass('active')
                 }
     
                 ACTIVE_TOP_HEADER = menu_1_item
-                menu_1_item.classList.add('active')
+                menu_1_item.addClass('active')
     
     
                 menu_1.html('')
