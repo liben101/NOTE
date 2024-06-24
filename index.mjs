9dceb4887e9d5055ea7432a4dd714bde6f9298f2
lb <714376485@qq.com>
2024-06-24 14:51:04 +0800

    1

--- index.mjs
+++ index.mjs
@@ -1,48 +1,55 @@
-const MENUS_1 = [
-    {
-        icon: ``,
-        title: '和利时DCS',
-        children: [
-            {
-                icon: ``,
-                title: '主控制器',
-                src1: './drawio/和利时_主控制器单元1.drawio.html'
-            }
-        ]
-    }, {
-        icon: ``,
-        title: 'MES相关',
-        children: [
-            {
-                icon: ``,
-                title: 'MES上下游',
-                src1: './drawio/MES上下游1.drawio.html'
-            },
-            {
-                icon: ``,
-                title: 'SQL相关',
-                src1: './drawio/SQL相关.drawio.html'
-            }
-        ]
-    }, {
-        icon: ``,
-        title: '中控DCS',
-        children: [
-            {
-                icon: ``,
-                title: '基本',
-                src1: './drawio/中控/组态软件基本.drawio.html'
-            }, {
-                icon: ``,
-                title: 'PID功能块的对比',
-                src1: './drawio/中控/PID功能块的对比.drawio.html'
-            },
+// const MENUS_1 = [
+//     {
+//         icon: ``,
+//         title: '和利时DCS',
+//         children: [
+//             {
+//                 icon: ``,
+//                 title: '主控制器',
+//                 src1: './drawio/和利时_主控制器单元1.drawio.html'
+//             }
+//         ]
+//     }, {
+//         icon: ``,
+//         title: 'MES相关',
+//         children: [
+//             {
+//                 icon: ``,
+//                 title: 'MES上下游',
+//                 src1: './drawio/MES上下游1.drawio.html'
+//             },
+//             {
+//                 icon: ``,
+//                 title: 'SQL相关',
+//                 src1: './drawio/SQL相关.drawio.html'
+//             }
+//         ]
+//     }, {
+//         icon: ``,
+//         title: '中控DCS',
+//         children: [
+//             {
+//                 icon: ``,
+//                 title: '基本',
+//                 src1: './drawio/中控/组态软件基本.drawio.html'
+//             }, {
+//                 icon: ``,
+//                 title: 'PID功能块的对比',
+//                 src1: './drawio/中控/PID功能块的对比.drawio.html'
+//             },
 
-        ]
-    },
-]
+//         ]
+//     },
+// ]
 
-var ACTIVE_TOP_HEADER = null
+
+fetch("./json/TOP_MENUS_SCHEMA.json").then(res=>{
+    return res.json()
+}).then(res=>{
+
+    var MENUS_1 = res
+
+    var ACTIVE_TOP_HEADER = null
 
 var iframe1 = document.getElementById('iframe1')
 
@@ -121,3 +128,5 @@ function initMenu1() {
 }
 
 initMenu1()
+})
+
